44f1fa54afc257e14e556f20e64f439f2496affe
alexandrevillegas <alex.villegas@gmail.com>
2025-06-19 21:12:55 -0300

    Entrega do sistema bancário

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -25,7 +25,7 @@ while True:
             extrato += f"Depósito: R$ {valor:.2f}\n"
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("Operação inválida! O valor informado é inválido.")
 
     elif opcao == "s":
         valor = float(input("Informe o valor do saque: "))
@@ -37,13 +37,13 @@ while True:
         excedeu_saques = numero_saques >= LIMITE_SAQUES
 
         if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
+            print("Operação não efetuada! Saldo insuficiente.")
 
         elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
+            print("Operação não efetuada! Valor do saque acima do limite.")
 
         elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
+            print("Operação não efetuada! Número máximo de saques excedido.")
 
         elif valor > 0:
             saldo -= valor
@@ -51,7 +51,7 @@ while True:
             numero_saques += 1
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("Operação não efetuada! O valor informado é inválido.")
 
     elif opcao == "e":
         print("\n================ EXTRATO ================")
